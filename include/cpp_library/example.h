1f1ba51e3dae963d07bc3ce731dc531276072419
moyac1012 <moyac@realidea.jp>
2023-10-02 00:34:37 +0900

    first commit

---
+++ include/cpp_library/example.h
@@ -0,0 +1,8 @@
+//
+// Created by 田森湧斗 on 2023/10/02.
+//
+
+#ifndef CPP_LIBRARY_EXAMPLE_H
+#define CPP_LIBRARY_EXAMPLE_H
+
+#endif //CPP_LIBRARY_EXAMPLE_H
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef CPP_LIBRARY_LIBRARY_H
+#define CPP_LIBRARY_LIBRARY_H
+
+void hello(void);
+
+#endif //CPP_LIBRARY_LIBRARY_H
